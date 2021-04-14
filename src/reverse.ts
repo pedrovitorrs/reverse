8ccce54b2672bd94dca341a7e74205933e43c7e3
pssilva22 <pedro442.rodrigues@gmail.com>
2021-04-13 22:06:33 -0300

    refactor: implementation reverse

--- src/reverse.ts
+++ src/reverse.ts
@@ -1,3 +1,5 @@
 export function reverse(original: string): string {
-  return original === '' : '' ? reverse(original.substr(1)) + original.charArt(0);
+  // return original === '' ? '' : reverse(original.substr(1)) + original.charAt(0);
+
+  return original.split('').reverse().join('');
 }
